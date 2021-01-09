4ed458133fe61772324ae30f292f3bdc4eb750f5
Nicolas Leroy <nicolas@devside.be>
2021-01-09 20:45:09 +0100

    submit keys to ENA in one call to not hit the quota #127

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/transaction/RetrieveDiagnosisKeysTransaction.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/transaction/RetrieveDiagnosisKeysTransaction.kt
@@ -272,22 +272,18 @@ object RetrieveDiagnosisKeysTransaction : Transaction() {
     /**
      * Executes the API_SUBMISSION Transaction State
      *
-     * We currently use Batch Size 1 and thus submit multiple times to the API.
-     * This means that instead of directly submitting all files at once, we have to split up
-     * our file list as this equals a different batch for Google every time.
+     * Sending all files at once because of google quota
      */
     private suspend fun executeAPISubmission(
         token: String,
         exportFiles: Collection<File>,
         exposureConfiguration: ExposureConfiguration?
     ) = executeState(API_SUBMISSION) {
-        exportFiles.forEach { batch ->
-            InternalExposureNotificationClient.asyncProvideDiagnosisKeys(
-                listOf(batch),
-                exposureConfiguration,
-                token
-            )
-        }
+        InternalExposureNotificationClient.asyncProvideDiagnosisKeys(
+            exportFiles,
+            exposureConfiguration,
+            token
+        )
         Timber.d("Diagnosis Keys provided successfully, Token: $token")
     }
 
